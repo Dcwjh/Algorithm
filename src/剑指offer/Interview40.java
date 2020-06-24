f8266cb14598ccd6e1cf39d16a8908f429a226d6
Dcwjh <503396265@qq.com>
2020-06-24 15:26:37 +0800

    剑指offer40题

--- src/剑指offer/Interview40.java
+++ src/剑指offer/Interview40.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 
 public class Interview40 {
-    //快速排序思想
+    //快速排序思想  复杂度：O(n)
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         ArrayList<Integer> list = new ArrayList<>();
         if(input == null || input.length ==0 )
@@ -62,4 +62,13 @@ public class Interview40 {
         array[left] = array[right];
         array[right] = temp;
     }
+
+    //方法二： 要求不能改变数组，采用红黑树.可以适用于海量数据（大根堆）
+    /**
+     * 创建k个大小的数组容器来存储这k个元素。
+     * 一开始：不断像容器中插入元素。
+     * 容器已满：1.在容器中找到最大数（大根堆，红黑树）
+     *          2. 删除最大数
+     *          3. 插入新数值
+     */
 }
