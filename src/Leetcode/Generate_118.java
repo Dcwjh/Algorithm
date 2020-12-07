5d9bbf5d75ce2f41e471191d18c76b411373fa7b
Dcwjh <503396265@qq.com>
2020-12-07 15:24:57 +0800

    update

---
+++ src/Leetcode/Generate_118.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author Jianhai Wang
+ * @ClassName Generate_118
+ * @Date 12/7/2020 9:39 AM
+ * @Version 1.0
+ */
+
+
+public class Generate_118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j ==i){
+                    list.add(1);
+                } else{
+                    list.add(result.get(i -1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(list);
+        }
+        return result;
+
+    }
+}
---
+++ src/Leetcode/IsPossible_659.java
@@ -0,0 +1,45 @@
+package Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * @Description TODO
+ * @Author Jianhai Wang
+ * @ClassName IsPossible_659
+ * @Date 12/4/2020 7:15 PM
+ * @Version 1.0
+ */
+
+
+public class IsPossible_659 {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<Integer, PriorityQueue<Integer>>();
+        for (int x : nums) {
+            if (!map.containsKey(x)) {
+                map.put(x, new PriorityQueue<Integer>());
+            }
+            if (map.containsKey(x - 1)) {
+                int prevLength = map.get(x - 1).poll();
+                if (map.get(x - 1).isEmpty()) {
+                    map.remove(x - 1);
+                }
+                map.get(x).offer(prevLength + 1);
+            } else {
+                map.get(x).offer(1);
+            }
+        }
+        Set<Map.Entry<Integer, PriorityQueue<Integer>>> entrySet = map.entrySet();
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : entrySet) {
+            PriorityQueue<Integer> queue = entry.getValue();
+            if (queue.peek() < 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+   
+}
